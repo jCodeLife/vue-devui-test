acae95656b289a874ab8e7b5a9e8c1bef37cc624
jCodeLife <bingjin.liao@outlook.com>
2022-04-21 11:06:35 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="alert-demo-1">
+    <d-alert type="success">success</d-alert>
+    <d-alert type="danger">danger</d-alert>
+    <d-alert type="warning">warning</d-alert>
+    <d-alert type="info">info</d-alert>
+    <d-alert type="simple">simple</d-alert>
+  </div>
+</template>
+<style>
+.alert-demo-1 .devui-alert {
+  margin-bottom: 20px;
+  font-size: 50px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import DevUI from 'vue-devui'
+import 'vue-devui/style.css'
+
+
+createApp(App).use(DevUI).mount('#app')
